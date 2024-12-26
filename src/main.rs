e8217451a4ed38256bbe34ea154918fb6b1ce3e3
aqyuki <aqyuki@aqyuki.dev>
2024-12-26 17:18:53 +0900

    ✨ 画面が表示されるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+#[derive(Default)]
+struct Application {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for Application {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        // event loopを作成
+        let window_attributes = Window::default_attributes().with_title("Original Game Engine!");
+        self.window = Some(event_loop.create_window(window_attributes).unwrap())
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // env_loggerを初期化
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut application = Application::default();
+    event_loop.run_app(&mut application).unwrap();
 }
