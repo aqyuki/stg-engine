803261498e7cc0102b83bfd647d9ae061501ca35
aqyuki <aqyuki@aqyuki.dev>
2024-12-26 17:30:57 +0900

    💡 Remove comment

--- src/main.rs
+++ src/main.rs
@@ -29,19 +29,6 @@ impl ApplicationHandler for Application {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
                 self.window.as_ref().unwrap().request_redraw();
             }
             _ => (),
